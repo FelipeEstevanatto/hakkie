b87f188657e0b754c506057b70329642e6c256e4
Felipe_Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2021-08-05 10:55:38 -0300

    Followers and following page now working

--- public/views/following.php
+++ public/views/following.php
@@ -7,18 +7,24 @@
     }
 
     require("../../app/database/connect.php");
+
     if ( !isset($_GET['user']) || !is_numeric($_GET['user'])) {
         include("../includes/user-nonexistent.php"); //This user does not exist in DB!
         exit();
-    } elseif ($_GET['user'] == $_SESSION['idUser']) {
-        $himself = true;
+    } else {
+
+        $GET_user = filter_var($_GET['user'], FILTER_VALIDATE_INT);
+
+        if ($_GET['user'] == $_SESSION['idUser']) {
+            $himself = true;
+        }
     }
 
     $query = "SELECT name_user, user_info, user_picture, user_banner, created_at, auth_type, darkmode FROM users WHERE id_user = :id_user";
 
     $stmt = $conn -> prepare($query);
 
-    $stmt -> bindValue(':id_user', $_GET['user']);
+    $stmt -> bindValue(':id_user', $GET_user);
 
     $stmt -> execute();
 
@@ -42,19 +48,20 @@
 
     $query = "SELECT user_followed, follow_date, fk_user FROM follows WHERE user_followed = :id_user ORDER BY follow_date";
     $stmt = $conn -> prepare($query);
-    $stmt -> bindValue(':id_user', $_GET['user']);
+    $stmt -> bindValue(':id_user', $GET_user);
     $stmt -> execute();
     $return = $stmt -> fetchAll(PDO::FETCH_ASSOC);
 
     $followers = count($return);
 
-    $query = "SELECT user_followed, follow_date, fk_user FROM follows WHERE fk_user = :id_user ORDER BY follow_date";
+    $query = "SELECT user_followed, follow_date, fk_user, name_user, user_picture, user_info, auth_type FROM follows 
+    INNER JOIN users ON id_user = user_followed WHERE fk_user = :id_user ORDER BY follow_date";
     $stmt = $conn -> prepare($query);
-    $stmt -> bindValue(':id_user', $_GET['user']);
+    $stmt -> bindValue(':id_user', $GET_user);
     $stmt -> execute();
-    $return = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    $data = $stmt -> fetchAll(PDO::FETCH_ASSOC);
 
-    $following = count($return);
+    $following = count($data);
 
 ?>
 
@@ -98,22 +105,61 @@
                 }
             ?>
 
-            <a href="user.php?user=<?=$_GET['user']?>"><?=$user_name?></a>
+            <a href="user.php?user=<?=$GET_user?>"><?=$user_name?></a>
          </div>
 
          <div class="tab-list">
-            <a href="following.php?user=<?=$_GET['user']?>" class="tab">
+            <a href="following.php?user=<?=$GET_user?>" class="tab">
                 (<?=$following?>) Following
                 <div class="underline"></div>
             </a>
-            <a href="followers.php?user=<?=$_GET['user']?>" class="tab">
+            <a href="followers.php?user=<?=$GET_user?>" class="tab">
                 (<?=$followers?>) Followers
             </a>
         </div>
 
          <div id="feed">
             <?php
-                //Request posts
+                foreach ($data as $users) {
+                    $each_user = '
+                            <div class="user-box">
+                        <div class="box-top">
+                            <div class="info">';
+                                if ($users['auth_type'] == 'GOOGLE') {
+                                    $each_user .='<img src="'.$users['user_picture'].'" alt="Picture of user">';
+                                } elseif (!is_null($user_picture)) {
+                                    echo '<img src="../images/'.$users['user_picture'].'.png" alt="Picture of user">';
+                                } else { //fallback
+                                    echo '<img src="../images/defaultUser.png">';
+                                }
+                        $each_user .='
+                                <a href="user.php?user='.$users['fk_user'].'">'.$users['name_user'].'</a>
+                            </div>
+                            
+                            <div class="btn follow">
+                                <i class="fas fa-user-plus"></i>
+                                <a href="user.php?user='.$users['fk_user'].'"><span>Follow</span></a>
+                            </div>
+                        </div>
+
+                        <div class="box-date">
+                            <span>Following since: '.str_replace('-','/',substr($users['follow_date'],0,10)).'</span>
+                        </div>
+
+                        <div class="box-about">';
+                        if (!is_null($users['user_info']))
+                            $each_user .= $users['user_info'];
+                        $each_user .='
+                        </div>
+
+                        <div class="btn-responsive">
+                            <i class="fas fa-user-plus"></i>
+                            <span>Follow</span>
+                        </div>
+                    </div>
+                    ';
+                    echo$each_user;
+                }
             ?>
             <!--Follow layout-->
             

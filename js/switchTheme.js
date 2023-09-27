b68b3df6284205e6a09c74c777d0f0339b331753
Felipe Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2023-09-27 01:53:19 -0300

    theme

--- js/switchTheme.js
+++ js/switchTheme.js
@@ -5,5 +5,12 @@ const body = window.document.querySelector('body');
 inputTheme.addEventListener('click', (value) => { 
     console.log('dark', value.target.checked)
 
-    body.classList.toggle('white-mode');
+    body.classList.toggle('dark');
+    if (value.target.checked) {
+        localStorage.setItem('theme', 'dark');
+        stateTheme.innerHTML = 'Dark';
+    } else {
+        localStorage.setItem('theme', 'light');
+        stateTheme.innerHTML = 'Light';
+    }
 }); 
